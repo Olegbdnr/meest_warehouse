94f9c86bc751eec1939d1db9c01d5e7b9b3d6e76
Olegbdnr <oleg.br.1304@gmail.com>
2023-12-11 22:47:51 +0200

    feat: add city-component; add general UX of application

---
+++ app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <div className="city-card"></div>
+        <div className="city-card"></div>
+        <div className="city-card"></div>
+        <div className="city-card"></div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
